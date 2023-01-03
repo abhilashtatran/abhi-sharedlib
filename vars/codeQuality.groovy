b17e7b19cb47a44c51ecec9c1a7a8ac797650716
abhilashtatran <116997780+abhilashtatran@users.noreply.github.com>
2023-01-03 18:57:23 +0530

    Update codeQuality.groovy

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call () {
 //   recordIssues(tools: [findBugs(pattern: '**/target/site/findbugs/findbugsXml.xml', useRankAsPriority: true)])
      recordIssues(tools: [pmdParser(pattern: '**/pmd.xml')])
 }
