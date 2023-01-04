08d7ca14d24af06d101c4aed75224003672d9cc6
abhilashtatran <116997780+abhilashtatran@users.noreply.github.com>
2023-01-04 18:37:17 +0530

    Update dependencyCheck.groovy

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,6 +1,6 @@
-def dependencyCheck(Map stepParams){
+def depCheck(Map stepParams){
   name="${stepParams.name}"
-//   depCheck additionalArguments: '', odcInstallation: "${name}"
-//                 dependencyCheckPublisher pattern: ''
-  echo "${name}"
+  dependencyCheck additionalArguments: '', odcInstallation: "${name}"
+                dependencyCheckPublisher pattern: ''
+//   echo "this is ${name}"
 }
