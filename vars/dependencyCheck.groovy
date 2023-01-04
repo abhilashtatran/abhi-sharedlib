9149197a055bd28bf22dad2a621cb979d927d478
abhilashtatran <116997780+abhilashtatran@users.noreply.github.com>
2023-01-05 00:24:15 +0530

    Update dependencyCheck.groovy

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -2,6 +2,6 @@ def depCheck (Map stepParams) {
   name="${stepParams.name}"
 //   sh 'mvn org.owasp:dependency-check-maven:7.4.3:check'
   
-  sh "dependencyCheck additionalArguments: '', odcInstallation: ${name}"
+  sh "dependencyCheck additionalArguments: '', odcInstallation: ${'name'}"
   dependencyCheckPublisher pattern: ''
 }
